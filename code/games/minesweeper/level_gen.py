c6d8b531e01e85b922cdcfee90d7b91d4e706e27
John Witulski <jowit001@hhu.de>
2026-06-27 23:27:40 +0200

    Add empty minesweeper skeleton

---
+++ code/games/minesweeper/level_gen.py
@@ -0,0 +1,72 @@
+from random import randrange
+WIDTH = 16
+HEIGHT = 12
+MINE_NUM = 20
+MINE_HEX_VALUE = 10
+board = [0]*(WIDTH*HEIGHT)
+
+# compute mine positions
+indices = [x for x in range(WIDTH*HEIGHT)]
+mine_positions = []
+for i in range(MINE_NUM):
+    pos = randrange(len(indices))
+    indices.pop(pos)
+    mine_positions.append(pos)
+
+# place mine positions
+for i in mine_positions:
+    board[i] = MINE_HEX_VALUE
+
+    # compute numbers
+for i in range(len(board)):
+    if board[i] == MINE_HEX_VALUE:
+        continue
+    
+    # compute possible positions (also handles spec. cases)
+    # p0 p1 p2
+    # p3  i p4
+    # p5 p6 p7
+    positions = []
+    if i % WIDTH !=0: 
+        p0 = i-WIDTH-1
+        if p0 >= 0:
+            positions.append(p0)
+        p3 = i-1
+        if p3 >= 0:
+            positions.append(p3)
+        p5 = i+WIDTH-1
+        if p5 < WIDTH*HEIGHT:
+            positions.append(p5)
+    if i % WIDTH != WIDTH-1:
+        p2 = i-WIDTH+1
+        if p2 >= 0:
+            positions.append(p2)
+        p4 = i+1
+        if p4 < WIDTH*HEIGHT:
+            positions.append(p4)
+        p7 = i+WIDTH+1
+        if p7 < WIDTH*HEIGHT:
+            positions.append(p7)
+    p1 = i-WIDTH
+    if p1 >= 0:
+        positions.append(p1)
+    p6 = i+WIDTH
+    if p6 < WIDTH*HEIGHT:
+        positions.append(p6)
+    # check all positions for mine
+    mine_counter = 0
+    for p in positions:
+        if board[p]==MINE_HEX_VALUE:
+            mine_counter +=1
+    board[i] = mine_counter
+
+# print game
+index = 0
+while index<WIDTH*HEIGHT:
+    if index % WIDTH == 0:
+        print()
+        print(".byte ", end='')
+    hex_value = hex(board[index])[2:]
+    print(f"$0{hex_value},", end='')
+    index += 1
+print()
---
+++ code/games/minesweeper/minesweeper.asm
@@ -0,0 +1,162 @@
+;ca65 minesweeper.asm -o t.o -t nes && ld65 t.o -o minesweeper.nes -t nes
+.segment "HEADER"
+.byte "NES"
+.byte $1a
+.byte $02 ; 2 * 16KB PRG ROM
+.byte $01 ; 1 * 8KB CHR ROM
+.byte %00000001 ; mapper and mirroring
+.byte $00
+.byte $00
+.byte $00
+.byte $00
+.byte $00, $00, $00, $00, $00 ; filler bytes
+.segment "ZEROPAGE" ; LSB 0 - FF
+.segment "STARTUP"
+Reset:
+    SEI ; Disables all interrupts
+    CLD ; disable decimal mode
+
+    ; Disable sound IRQ (for some reason everthing is broken without this line)
+    LDX #$40
+    STX $4017
+
+    ; Initialize the stack register
+    LDX #$FF
+    TXS
+
+;   INX ; #$FF + 1 => #$00
+    LDX #$00
+
+    ; Zero out the PPU registers
+    STX $2000
+    STX $2001
+
+    STX $4010
+
+:
+    BIT $2002 ; wait for vblank
+    BPL :-
+
+;     ;TXA
+    LDA #$00
+
+CLEARMEM:
+    STA $0000, X ; $0000 => $00FF
+    STA $0100, X ; $0100 => $01FF
+    STA $0300, X
+    STA $0400, X
+    STA $0500, X
+    STA $0600, X
+    STA $0700, X
+    LDA #$FF
+    STA $0200, X ; $0200 => $02FF
+    LDA #$00
+    INX
+    BNE CLEARMEM    
+; wait for vblank
+:
+    BIT $2002
+    BPL :-
+
+    LDA #$02  ; high byte of sprites
+    STA $4014
+    NOP
+
+    ; $3F00
+    LDA #$3F
+    STA $2006
+    LDA #$00
+    STA $2006
+
+    LDX #$00
+LoadPalettes:
+    LDA PaletteData, X
+    STA $2007 ; $3F00, $3F01, $3F02 => $3F1F
+    INX
+    CPX #$20 ; dezi 32
+    BNE LoadPalettes    
+
+    LDX #$00
+LoadSprites:
+    LDA SpriteData, X
+    STA $0200, X
+    INX
+    CPX #$10
+    BNE LoadSprites    
+
+; Clear the nametables- this isn't necessary in most emulators unless
+; you turn on random memory power-on mode, but on real hardware
+; not doing this means that the background / nametable will have
+; random garbage on screen. This clears out nametables starting at
+; $2000 and continuing on to $2400 (which is fine because we have
+; vertical mirroring on. If we used horizontal, we'd have to do
+; this for $2000 and $2800)
+    LDX #$00
+    LDY #$00
+    LDA $2002
+    LDA #$20
+    STA $2006
+    LDA #$00
+    STA $2006
+ClearNametable:
+    STA $2007
+    INX
+    BNE ClearNametable
+    INY
+    CPY #$08
+    BNE ClearNametable
+    
+    ; Enable interrupts
+    CLI
+
+    LDA #0
+    STA $2005 ; X position (this also sets the w register)
+    STA $2005 ; Y position (this also clears the w register)
+GameLoop:
+    ; main game code
+    JMP GameLoop
+
+NMI:
+    PHA 
+    TXA
+    PHA
+    TYA
+    PHA
+
+    PLA
+    TAY
+    PLA
+    TAX
+    PLA
+    RTI
+
+PaletteData: ; maxvalue 0x36
+  .byte $3F,$20,$1A,$0F, $3F,$10,$1A,$0F, $3F,$30,$21,$0f, $3F,$27,$17,$0F  ;background palette data
+  .byte $3F,$16,$27,$18, $3F,$1A,$30,$27, $3F,$16,$30,$27, $3F,$0F,$36,$17  ;sprite palette data
+
+SpriteData:
+  .byte $08, $01, $01, $08 ; Y,Tileindex, ATTR, X
+  .byte $08, $02, $01, $10
+  .byte $10, $03, $01, $08
+  .byte $10, $04, $01, $10
+
+Level01:
+    .byte $01,$01,$00,$00,$00,$00,$00,$00,$00,$00,$01,$0a,$01,$00,$00,$00
+    .byte $0a,$01,$00,$01,$02,$02,$01,$01,$01,$01,$02,$02,$02,$00,$00,$00
+    .byte $01,$01,$00,$01,$0a,$0a,$01,$01,$0a,$01,$01,$0a,$02,$01,$01,$00
+    .byte $00,$00,$00,$02,$03,$03,$01,$01,$01,$01,$01,$01,$02,$0a,$01,$00
+    .byte $00,$00,$00,$01,$0a,$02,$01,$01,$01,$01,$01,$00,$01,$01,$01,$00
+    .byte $00,$00,$00,$01,$01,$02,$0a,$01,$01,$0a,$01,$01,$01,$02,$01,$01
+    .byte $00,$00,$00,$00,$00,$01,$01,$01,$01,$02,$02,$02,$0a,$02,$0a,$01
+    .byte $00,$00,$00,$00,$00,$01,$01,$01,$00,$02,$0a,$03,$01,$02,$01,$01
+    .byte $00,$00,$00,$00,$01,$02,$0a,$01,$01,$03,$0a,$03,$01,$01,$01,$01
+    .byte $00,$00,$00,$00,$01,$0a,$02,$01,$01,$0a,$03,$0a,$01,$01,$0a,$01
+    .byte $01,$01,$00,$00,$01,$01,$01,$00,$01,$01,$02,$01,$01,$01,$01,$01
+    .byte $0a,$01,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
+
+.segment "VECTORS"
+    .word NMI
+    .word Reset
+    ; 
+.segment "CHARS"
+    .incbin "tilemap.chr"
