e7567e8925822167a90e16bba071d0ec3111a585
John Witulski <jowit001@hhu.de>
2026-06-28 20:02:30 +0200

    Draw full board with mines and numbers (1-4)

--- code/games/minesweeper/drawBoard.asm
+++ code/games/minesweeper/drawBoard.asm
@@ -13,10 +13,30 @@ drawTileTop:
     JSR _levelTileToA
     CMP #$01
     BEQ _1Top
+    CMP #$02
+    BEQ _2Top
+    CMP #$03
+    BEQ _3Top
+    CMP #$04
+    BEQ _4Top
+    CMP #$0A
+    BEQ _MineTop
     JSR _drawHiddenTop
     JMP drawTileTopEnd
 _1Top:
     JSR _draw1Top
+    JMP drawTileTopEnd
+_2Top:
+    JSR _draw2Top
+    JMP drawTileTopEnd
+_3Top:
+    JSR _draw3Top
+    JMP drawTileTopEnd
+_4Top:
+    JSR _draw4Top
+    JMP drawTileTopEnd
+_MineTop:
+    JSR _drawMineTop
     JMP drawTileTopEnd
 drawTileTopEnd:
     INX
@@ -35,10 +55,30 @@ drawTileBottom:
     JSR _levelTileToA
     CMP #$01
     BEQ _1Bottom
+    CMP #$02
+    BEQ _2Bottom
+    CMP #$03
+    BEQ _3Bottom
+    CMP #$04
+    BEQ _4Bottom
+    CMP #$0A
+    BEQ _MineBottom
     JSR _drawHiddenBottom
     JMP drawTileBottomEnd
 _1Bottom:
     JSR _draw1Bottom
+    JMP drawTileBottomEnd
+_2Bottom:
+    JSR _draw2Bottom
+    JMP drawTileBottomEnd
+_3Bottom:
+    JSR _draw3Bottom
+    JMP drawTileBottomEnd
+_4Bottom:
+    JSR _draw4Bottom
+    JMP drawTileBottomEnd
+_MineBottom:
+    JSR _drawMineBottom
     JMP drawTileBottomEnd
 drawTileBottomEnd:   
     INX
@@ -56,6 +96,8 @@ EndOfDraw:
 
 ; assumes Y is the pointer
 _levelTileToA:
+    ; TODO: check VISIBEL bit array at pos Y
+    ; return empty tile if the tile is not visible
     LDA LEVEL_PTR
     CMP #$01
     BEQ Level1
@@ -82,6 +124,20 @@ _drawHiddenBottom:
     STA $2007
     RTS
 
+_drawMineTop:
+    LDA #$54
+    STA $2007
+    LDA #$55
+    STA $2007 
+    RTS
+
+_drawMineBottom:
+    LDA #$64
+    STA $2007
+    LDA #$65
+    STA $2007
+    RTS
+
 _draw1Top:
     LDA #$30
     STA $2007
@@ -93,5 +149,47 @@ _draw1Bottom:
     LDA #$40
     STA $2007
     LDA #$41
+    STA $2007
+    RTS
+
+_draw2Top:
+    LDA #$32
+    STA $2007
+    LDA #$33
+    STA $2007 
+    RTS
+
+_draw2Bottom:
+    LDA #$42
+    STA $2007
+    LDA #$43
+    STA $2007
+    RTS
+
+_draw3Top:
+    LDA #$34
+    STA $2007
+    LDA #$35
+    STA $2007 
+    RTS
+
+_draw3Bottom:
+    LDA #$44
+    STA $2007
+    LDA #$45
+    STA $2007
+    RTS
+
+_draw4Top:
+    LDA #$36
+    STA $2007
+    LDA #$37
+    STA $2007 
+    RTS
+
+_draw4Bottom:
+    LDA #$46
+    STA $2007
+    LDA #$47
     STA $2007
     RTS
